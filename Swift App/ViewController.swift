b50192e54a01c656679cdc7e9de843a5d2db380a
Jake Messenger <jake.messenger@mac.com>
2017-03-19 19:58:06 +0000

    Text fields added

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -9,29 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var theLabel: UILabel!
     
-    var tapCount = 0
+    @IBOutlet weak var text1: UITextField!
+    
+    @IBOutlet weak var text2: UITextField!
     
     @IBAction func buttonTapped(_ sender: Any) {
-       tapCount = tapCount + 1
-        if tapCount >= 20 {
-            theLabel.text = "20 times!!"
-        }
+        
+        print(text1.text!)
+        print(text2.text!)
     }
- 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
